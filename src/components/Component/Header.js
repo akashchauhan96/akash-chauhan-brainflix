e20dbab9f0d5eb7b5c77a1d8f39ff852805e0319
Akash Chauhan <akash.sc96@gmail.com>
2022-11-03 12:51:42 -0400

    Add icons folder, create initial element structure inside Header js, and add populate partial files with variables, mixins, and global styling

---
+++ src/components/Component/Header.js
@@ -0,0 +1,33 @@
+import "./Header.scss"
+import logo from '../../assets/logo/BrainFlix-logo.svg'
+import searchIcon from '../../assets/icons/search.svg'
+import avatarImage from '../../assets/images/Mohan-muruge.jpg'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img
+          className="header__logo-img"
+          src={logo}
+          alt="BrainFlix logo image"
+        />
+        <div className="search-box">
+          <img
+            className="search-box__icon"
+            src={searchIcon}
+            alt="Search icon image for search box"
+          />
+          <p className="searh-box__placeholder-text">Search</p>
+        </div>
+      </div>
+      <img
+        className="header__avatar-image"
+        src={avatarImage}
+        alt="Avatar Image of Mohan Muruge"
+      />
+    </header>
+  )
+}
+
+export default Header
