81cc61b4857eaa94d58cca31a48efd76e22e71f9
Akash Chauhan <akash.sc96@gmail.com>
2022-11-03 22:07:14 -0400

    Finish first draft header section for all viewport sizes but might need to refactor components later on

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,41 @@ import "./Header.scss"
 import logo from '../../assets/logo/BrainFlix-logo.svg'
 import searchIcon from '../../assets/icons/search.svg'
 import avatarImage from '../../assets/images/Mohan-muruge.jpg'
+import uploadIcon from '../../assets/icons/upload.svg'
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__container">
-        <div>
+      <div className="header__outer-container">
         <img
           className="header__logo-img"
           src={logo}
           alt="BrainFlix logo image"
         />
-        </div>
-        <div className="search-box">
+        <div className="header__inner-container">
+          <div className="header__search-box">
+            <img
+              className="header__search-icon"
+              src={searchIcon}
+              alt="Search icon image for search box"
+            />
+            <p className="header__search-text">Search</p>
+          </div>
           <img
-            className="search-box__icon"
-            src={searchIcon}
-            alt="Search icon image for search box"
+            className="header__avatar-image"
+            src={avatarImage}
+            alt="Avatar Image of Mohan Muruge"
           />
-          <p className="searh-box__placeholder-text">Search</p>
+          <button className="header__upload-button">
+            <img 
+              className="header__upload-icon" 
+              src={uploadIcon}
+              alt="Upload Icon"
+            />
+            <span className="header__upload-text">Upload</span>
+          </button>
         </div>
       </div>
-      <img
-        className="header__avatar-image"
-        src={avatarImage}
-        alt="Avatar Image of Mohan Muruge"
-      />
     </header>
   )
 }
