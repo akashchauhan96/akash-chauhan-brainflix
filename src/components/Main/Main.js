24dce85a40269df03be9500199e7a0fb4d84a832
Akash Chauhan <akash.sc96@gmail.com>
2022-11-05 12:19:36 -0400

    Import HeroVideo, VideoDescription, and Comment to Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,7 @@
 import './Main.scss'
+import HeroVideo from '../HeroVideo/HeroVideo.js'
+import VideoDescription from '../VideoDescription/VideoDescription.js'
+import Comment from '../Comment/Comment.js'
 
 function Main() {
   return (
