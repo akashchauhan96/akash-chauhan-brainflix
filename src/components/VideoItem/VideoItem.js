99c72cf2458c9cf2be31ee375cf2e4e704a985f3
Akash Chauhan <akash.sc96@gmail.com>
2022-11-13 22:26:06 -0500

    Add a div container around img tag this way it made it easier to make all the image sizes equal for rec videos

--- src/components/VideoItem/VideoItem.js
+++ src/components/VideoItem/VideoItem.js
@@ -8,7 +8,9 @@ export default function VideoItem(props) {
     <>
       <Link to={`/videos/${id}`} className="recommended-videos__video-link">
         <div className='recommended-videos__single-wrapper' >
-          <img src={image} alt={title} className='recommended-videos__videos'/>
+          <div className='recommended-videos__video-box'>
+            <img src={image} alt={title} className='recommended-videos__videos'/>
+          </div>
           <div className='recommended-videos__video-info'>
             <p className='recommended-videos__video-title'>{title}</p>
             <p className='recommended-videos__video-channel'>{channel}</p>
