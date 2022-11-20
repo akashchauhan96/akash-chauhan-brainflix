d291c242f332a82bef50b6c871bfc8162259dda4
Akash Chauhan <akash.sc96@gmail.com>
2022-11-20 15:51:15 -0500

    Add main url to api-utils folder to be later refactored to env file

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -1,7 +1,7 @@
 //Deconstruct axious url link
-// const mainURL = 'https://project-2-api.herokuapp.com/'
+const mainURL = 'http://localhost:8080/'
 const videosEndpoint = 'videos/'
 // const apiKey = '?api_key=cd79fcde-0845-4d56-ab55-a675677dca40'
 
-export const getVideosArray = () => videosEndpoint;
-export const getVideoDetailsById = (id) => videosEndpoint + id;
+export const getVideosArray = () => mainURL + videosEndpoint;
+export const getVideoDetailsById = (id) => mainURL + videosEndpoint + id;
