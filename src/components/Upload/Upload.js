356109decb8b1b5429982993c1911687b8c4ea4c
Akash Chauhan <akash.sc96@gmail.com>
2022-11-20 15:33:25 -0500

    Add handleSubmit function to capture information from form submission

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,12 +1,30 @@
 import './Upload.scss'
 import videoPreview from '../../assets/images/Upload-video-preview.jpg'
-import { Link } from 'react-router-dom'
 import publishIcon from '../../assets/icons/publish.svg'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Upload() {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newVidName = e.target[0].value;
+    const newVidDescription = e.target[1].value;
+  }
+  
+  const handleCancel = (e) => {
+    e.preventDefault();
+    navigate('/');
+  }
+
   return (
     <main>
-      <section className='upload'>
+      <form className='upload' onSubmit={handleSubmit}>
         <h1 className='upload__title'>Upload Video</h1>
         <div className='video-main'>
           <figure className='video-main__video-figure'>
@@ -18,8 +36,12 @@ export default function Upload() {
               <label className='video-main__title-label'>Title Your Video</label>
               <input
                 type='text' 
-                name='video-title' className='video-main__title-input'
-                placeholder='Add a title to your video'>
+                name='video-title' 
+                className='video-main__title-input'
+                placeholder='Add a title to your video'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              >
               </input>
             </div>
             <div className='video-main__description-box'>
@@ -28,26 +50,24 @@ export default function Upload() {
                 name="video-description"
                 className="video-main__description-input"
                 placeholder="Add a description to your video"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </div>
           </div>  
         </div>
         <div className='button-section'>
-          <Link to='/upload/complete' style={{textDecoration: 'none'}}>
-            <button className="button-section__publish">
-              <img 
-                className="button-section__publish-icon" 
-                src={publishIcon}
-                alt="Publish Icon"
-              />
-              <span className="button-section__publish-text">Publish</span>
-            </button>
-          </Link>
-          <Link to='/' style={{textDecoration: 'none'}}>
-            <button className='button-section__cancel'>Cancel</button>
-          </Link>
+          <button className="button-section__publish" type="submit">
+            <img 
+              className="button-section__publish-icon" 
+              src={publishIcon}
+              alt="Publish Icon"
+            />
+            <span className="button-section__publish-text">Publish</span>
+          </button>
+          <button className='button-section__cancel' onClick={handleCancel}>Cancel</button>
         </div>
-      </section>
+      </form>
     </main>
   )
 }
